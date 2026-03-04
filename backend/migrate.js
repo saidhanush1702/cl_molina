ccf6f4d92331e73250781b00266079363265166e
sai dhanush <dhany.tdsn@gmail.com>
2026-03-04 18:08:18 +0530

    updates the port details in backend

--- backend/migrate.js
+++ backend/migrate.js
@@ -17,6 +17,7 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
     multipleStatements: true
 });
 
